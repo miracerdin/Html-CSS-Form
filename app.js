484351f587715fd3828acc19b0fa51a289ff69d5
mirac <miracerdin@gmail.com>
2022-05-27 23:16:04 +0300

    app functions added

--- app.js
+++ app.js
@@ -15,36 +15,55 @@
 //   }
 // }
 //* kick the first element and sum rest of the numbers.
-let ls = [0, 1, 3, 6, 10];
-let result = [];
-let a;
-let total = 0;
-function recurse(a) {
-  for (let i = 0; i < ls.length; i++) {
-    total += ls[i];
-    //   ls.shift();
-  }
-  result.push(total);
-  let ls = ls.shift();
+// let ls = [0, 1, 3, 6, 10];
+// let result = [];
+// let a;
+// let total = 0;
+// function recurse(a) {
+//   for (let i = 0; i < ls.length; i++) {
+//     total += ls[i];
+//     //   ls.shift();
+//   }
+//   result.push(total);
+//   let ls = ls.shift();
 
-  if (ls.length == 0) {
-    return result;
-  } else {
-    recurse(ls);
-  }
-}
+//   if (ls.length == 0) {
+//     return result;
+//   } else {
+//     recurse(ls);
+//   }
+// }
 
-console.log(result);
-console.log(ls);
+// console.log(result);
+// console.log(ls);
 
-function partsSums(ls) {
-  let newList = [];
-  let total = ls.reduce((a, b) => a + b, 0);
-  if (ls.length <= 0) {
-    newList.push(total);
-    return newList;
-  }
-  newList.push(total);
-  ls.shift();
-  return newList.concat(partsSums(ls));
+// function partsSums(ls) {
+//   let newList = [];
+//   let total = ls.reduce((a, b) => a + b, 0);
+//   if (ls.length <= 0) {
+//     newList.push(total);
+//     return newList;
+//   }
+//   newList.push(total);
+//   ls.shift();
+//   return newList.concat(partsSums(ls));
+// }
+
+// let plus1 = document.getElementsByClassName("plus")[0];
+// let plus2 = document.getElementsByClassName("plus")[1];
+// let minus1 = document.getElementsByClassName("minus")[0];
+// let minus2 = document.getElementsByClassName("minus")[1];
+
+let toplam1 = Number(document.getElementsByClassName("ürün1")[0].innerHTML);
+console.log(toplam1);
+// let toplam2 = document.getElementsByClassName("ürün2")[0].innerHTML;
+
+// minus1.addEventListener("click", cıkarma);
+// function cıkarma() {
+//   toplam1--;
+// }
+
+function minus() {
+  toplam1 -= 1;
+  toplam1.toString();
 }
