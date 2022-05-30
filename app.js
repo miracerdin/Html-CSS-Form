d85ce64d2a060757e9fe33fb13a5794b80dc9246
mirac <miracerdin@gmail.com>
2022-05-30 22:58:29 +0300

    button Js added

--- app.js
+++ app.js
@@ -54,16 +54,118 @@
 // let minus1 = document.getElementsByClassName("minus")[0];
 // let minus2 = document.getElementsByClassName("minus")[1];
 
-let toplam1 = Number(document.getElementsByClassName("ürün1")[0].innerHTML);
-console.log(toplam1);
+// let toplam1 = Number(document.getElementsByClassName("ürün1")[0].innerHTML);
+// console.log(toplam1);
 // let toplam2 = document.getElementsByClassName("ürün2")[0].innerHTML;
 
 // minus1.addEventListener("click", cıkarma);
 // function cıkarma() {
 //   toplam1--;
 // }
+// const cost = document.getElementsByClassName("cost")[0].innerHTML.slice(1);
+// function arttir() {
+//   var sonuc = document.getElementById("sonuc").innerText;
+//   sonuc = Number(sonuc) + 1;
+//   document.getElementById("sonuc").innerText = sonuc;
+//   console.log(document.getElementsByClassName("cost")[0].innerHTML);
+//   cost2 = 54.99 * sonuc;
+//   console.log(cost2);
+//   document.getElementsByClassName("cost")[0].innerHTML = "$" + cost2.toString();
+//   console.log(sonuc);
+// }
 
-function minus() {
-  toplam1 -= 1;
-  toplam1.toString();
+// function azalt() {
+//   var sonuc = document.getElementById("sonuc").innerText;
+//   sonuc = Number(sonuc) - 1;
+//   document.getElementById("sonuc").innerText = sonuc;
+//   cost2 = 54.99 * sonuc;
+//   document.getElementsByClassName("cost")[0].innerHTML = "$" + cost2.toString();
+//   if (document.getElementById("sonuc").innerText <= 0) {
+//     return (
+//       (document.getElementById("sonuc").innerText = 0),
+//       (document.getElementsByClassName("cost")[0].innerHTML = "$54.99")
+//     );
+//   }
+// }
+
+// const cost1 = document.getElementsByClassName("cost")[1].innerHTML.slice(1);
+// function arttir() {
+//   var sonuc = document.getElementById("sonuc2").innerText;
+//   sonuc = Number(sonuc) + 1;
+//   document.getElementById("sonuc2").innerText = sonuc;
+//   console.log(document.getElementsByClassName("cost")[1].innerHTML);
+//   cost2 = 54.99 * sonuc;
+//   console.log(cost2);
+//   document.getElementsByClassName("cost")[1].innerHTML = "$" + cost2.toString();
+//   console.log(sonuc);
+// }
+
+// function azalt() {
+//   var sonuc = document.getElementById("sonuc2").innerText;
+//   sonuc = Number(sonuc) - 1;
+//   document.getElementById("sonuc2").innerText = sonuc;
+//   cost2 = 54.99 * sonuc;
+//   document.getElementsByClassName("cost")[1].innerHTML = "$" + cost2.toString();
+//   if (document.getElementById("sonuc2").innerText <= 0) {
+//     return (
+//       (document.getElementById("sonuc2").innerText = 0),
+//       (document.getElementsByClassName("cost")[1].innerHTML = "$54.99")
+//     );
+//   }
+// }
+
+let incrementButton = document.getElementsByClassName("inc");
+let decrementButton = document.getElementsByClassName("dec");
+
+console.log(decrementButton);
+for (let i = 0; i < incrementButton.length; i++) {
+  let button = incrementButton[i];
+  button.addEventListener("click", function (event) {
+    let buttonClicked = event.target;
+    console.log(buttonClicked);
+    let incrementNumber = buttonClicked.parentElement.children[1].innerHTML;
+    console.log(incrementNumber);
+    // console.log(typeof incrementNumber);
+    let newincrementNumber = parseInt(incrementNumber) + 1;
+    // incrementNumber = newincrementNumber;
+    buttonClicked.parentElement.children[1].innerText = newincrementNumber;
+    console.log(newincrementNumber);
+
+    //** Cost seciton */
+    let input = buttonClicked.parentElement.parentElement.children[1];
+    let inputInnertext = input.innerText.slice(1);
+    let newValue = parseFloat(inputInnertext).toFixed(2);
+    let lastValue = inputInnertext * newincrementNumber;
+    buttonClicked.parentElement.parentElement.children[1].innerText =
+      "$" + lastValue;
+  });
+}
+// Decrement
+
+for (let i = 0; i < decrementButton.length; i++) {
+  let button = decrementButton[i];
+  button.addEventListener("click", function (event) {
+    let buttonClicked = event.target;
+    console.log(buttonClicked);
+    let incrementNumber = buttonClicked.parentElement.children[1].innerHTML;
+    console.log(incrementNumber);
+    // console.log(typeof incrementNumber);
+    let newincrementNumber = parseInt(incrementNumber) - 1;
+    // incrementNumber = newincrementNumber;
+    buttonClicked.parentElement.children[1].innerText = newincrementNumber;
+    console.log(newincrementNumber);
+    // Cost seciton
+    let input = buttonClicked.parentElement.parentElement.children[1];
+    let inputInnertext = input.innerText.slice(1);
+    console.log(inputInnertext);
+    let newValue = parseFloat(inputInnertext).toFixed(2);
+    let lastValue = inputInnertext * newincrementNumber;
+    console.log(lastValue);
+    buttonClicked.parentElement.parentElement.children[1].innerText =
+      "$" + lastValue;
+    if (lastValue <= 0) {
+      return (buttonClicked.parentElement.parentElement.children[1].innerText =
+        "$" + 0);
+    }
+  });
 }
